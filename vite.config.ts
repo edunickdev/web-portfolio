06e74c0db4156b843d394b8bae42c8443d721955
El Código Recuerda <114629663+edunickdev@users.noreply.github.com>
2024-09-14 12:58:18 -0500

    mime types

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   build: {
-    outDir: 'build', // Cambia el directorio de salida a 'build'
+    outDir: 'build',
   },
 })
