e718b4a686cd3c38f7ede04a5be36c9f4585e4a9
El Código Recuerda <114629663+edunickdev@users.noreply.github.com>
2024-09-17 21:38:58 -0500

    Update AboutMeScreen.tsx

--- src/pages/screens/AboutMeScreen.tsx
+++ src/pages/screens/AboutMeScreen.tsx
@@ -47,7 +47,7 @@ const AboutMeScreen = () => {
           >
             {(item) => (
               <Tab key={item.id} title={item.label}>
-                <Card className="h-52 xl:h-80">
+                <Card className="h-44 xl:h-80">
                   <CardBody>{item.content}</CardBody>
                 </Card>
               </Tab>
